81247df695b49eda936adfc26011a6dcd991e6ec
Glaydson <glaydsonvs@gmail.com>
2019-09-03 21:22:03 -0300

    Commit inicial

---
+++ src/main/java/br/edu/unichristus/LivrariaDataApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unichristus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaDataApplication.class, args);
+	}
+
+}
